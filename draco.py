d7b2f35b64aa0ad16341faf8509d32ed56c95214
Kun-fy <kosakatakun@gmail.com>
2026-02-07 19:36:42 +0700

    Update draco.py

--- draco.py
+++ draco.py
@@ -87,9 +87,8 @@ def initHeaders():
 
 def handleStatusCodes(status_code):
 	global request_counter
-	sys.stdout.write(f"\r[ %i ] >" % request_counter)
-	print(f"\r\033[38;5;220m Request | \033[31mstatus code \033[37" +str(status_code)+ " \033[38;5;220m|")
-	print(f"\r\033[32mpayload \033[33m" +str(payload)+ "\033[0m")
+	sys.stdout.write(f"\r[ %i ] > \033[32mRequest" % request_counter)
+	print(f"\r \033[31mstatus code \033[38;5;220m| " +status_code+ " | \033[37" +str(payload)+ "")
 	sys.stdout.flush()
 	if status_code == 429:
 			printMsg("You have been throttled")
